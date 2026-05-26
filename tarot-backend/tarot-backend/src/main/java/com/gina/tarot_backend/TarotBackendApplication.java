069d44621d5e8dfddcc07e09acb945d5f3360c33
gina <ginameiyee@gmail.com>
2026-05-26 11:57:00 -0400

    initial frontend and backend setup

---
+++ tarot-backend/tarot-backend/src/main/java/com/gina/tarot_backend/TarotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gina.tarot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarotBackendApplication.class, args);
+	}
+
+}
---
+++ tarot-backend/tarot-backend/src/test/java/com/gina/tarot_backend/TarotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gina.tarot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
