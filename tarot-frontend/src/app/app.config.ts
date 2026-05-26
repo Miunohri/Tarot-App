1398643d89645d1ddd79f822b53ed9fccad84c94
gina <ginameiyee@gmail.com>
2026-05-26 13:58:28 -0400

    adds tarot reading to frontend

--- tarot-frontend/src/app/app.config.ts
+++ tarot-frontend/src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    importProvidersFrom(FormsModule),
+    provideHttpClient()
   ]
 };
--- tarot-frontend/src/app/app.ts
+++ tarot-frontend/src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('tarot-frontend');
+
+  question = '';
+  result: any;
+
+  constructor(private http: HttpClient) {}
+
+  getReading() {
+    this.http.post('http://localhost:8080/reading', {
+      question: this.question
+    }).subscribe((res) => {
+      this.result = res;
+    });
+  }
 }
