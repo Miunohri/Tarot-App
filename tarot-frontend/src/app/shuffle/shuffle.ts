352c103fc3a4cca4c2561655bac06bb9e7da186f
gina <ginameiyee@gmail.com>
2026-05-28 15:12:58 -0400

    modifications made to shuffle component and tarot-deck service

--- tarot-frontend/src/app/shuffle/shuffle.ts
+++ tarot-frontend/src/app/shuffle/shuffle.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TarotState } from '../tarot-state';
+import { Router } from '@angular/router';
+import { TarotDeck } from '../tarot-deck';
 
 @Component({
   selector: 'app-shuffle',
@@ -6,4 +9,16 @@ import { Component } from '@angular/core';
   templateUrl: './shuffle.html',
   styleUrl: './shuffle.css',
 })
-export class Shuffle {}
+export class Shuffle {
+
+  constructor(public state: TarotState, private router: Router, private deck: TarotDeck) {}
+
+  shuffle() {
+    this.deck.shuffle();
+  }
+
+  cutDeck() {
+    this.deck.cutDeck();
+  }
+
+}
--- tarot-frontend/src/app/tarot-deck.ts
+++ tarot-frontend/src/app/tarot-deck.ts
@@ -3,4 +3,27 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class TarotDeck {}
+export class TarotDeck {
+  
+  deck: string[] = [];
+
+  createDeck() {
+    this.deck = [
+      'the fool',
+      'the magician',
+      'the high priestess'
+    ];
+  }
+
+  shuffle() {
+    for (let i = this.deck.length -1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+    }
+  }
+
+  cutDeck() {
+    const mid = Math.floor(this.deck.length / 2);
+    this.deck = []
+  }
+}
