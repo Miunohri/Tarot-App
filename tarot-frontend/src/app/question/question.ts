638104cfcb66eefa2977852154aab68b24ff8996
gina <ginameiyee@gmail.com>
2026-05-28 14:34:08 -0400

    updates to question component for routing and input

--- tarot-frontend/src/app/question/question.ts
+++ tarot-frontend/src/app/question/question.ts
@@ -1,9 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-question',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './question.html',
   styleUrl: './question.css',
 })
-export class Question {}
+export class Question {
+
+  constructor (private router: Router) {}
+
+  goToShuffle() {
+    this.router.navigate(['/shuffle'])
+  }
+
+  question = '';
+  result: any;
+
+
+}
